150c2f3594a8eff157c75d2c0e98581684d1f9d1
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2023-12-14 01:30:43 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,1 +1,14 @@
+provider "azurerm" {
+  features {}
+}
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "main-infra"
+    storage_account_name = "mydemotfstate"
+    container_name       = "tfstate"
+    key                  = "terraform-base.tfstate"
+  }
+}
+
+data "azurerm_client_config" "current" {}
